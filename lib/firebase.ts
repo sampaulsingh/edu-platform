395bdfc887b138dc5aedad7520eb9bd75a4b51bb
sampaulsingh <sampaulsingh@gmail.com>
2026-03-30 16:51:11 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,14 +2,27 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBgWD1qjovRb_Tb1f-Omu6QLIl-Zts9Mxg",
+  authDomain: "edu-platform-847c3.firebaseapp.com",
+  projectId: "edu-platform-847c3",
+  storageBucket: "edu-platform-847c3.firebasestorage.app",
+  messagingSenderId: "428909636453",
+  appId: "1:428909636453:web:479caff0c3aaa5c2eb4f8c",
+  measurementId: "G-60WYX92ZKM"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
